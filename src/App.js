92bd63cec58a341506299d2d37f0b19c0a7a736d
Aswin-web <ashvinachu097@gmail.com>
2023-03-13 10:30:19 +0530

    Homepage user routes added

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Route,Routes } from 'react-router-dom'
+import { Link, Route,Routes } from 'react-router-dom'
 import JobseekerPage from './Pages/JobSeeker Page/JobseekerPage';
 import "./Pages/JobSeeker Page/JobSeeker_Page_index.css"
 import  ApplicationStatusComponent from  './Pages/JobSeeker Page/components/ApplicationStatusComponent'
@@ -12,7 +12,14 @@ function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path="/" element={"<h1>Hello</h1>"} />
+        <Route
+          path="/"
+          element={
+            <div>
+              <h1>Hello</h1> <Link to="/user">USER </Link>
+            </div>
+          }
+        />
         <Route path="/user" element={<JobseekerPage />}>
           <Route index element={<ApplicationStatusComponent />} />
           <Route path="schedule" element={<UserSchduleComponent />} />
