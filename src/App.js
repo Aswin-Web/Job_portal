959b0206b0cee9fdeb1fb0f352a13dd3cf5795a5
Aswin-web <ashvinachu097@gmail.com>
2023-03-14 12:59:20 +0530

    Collaboration Updated

--- src/App.js
+++ src/App.js
@@ -1,44 +1,22 @@
-import { Link, Route,Routes } from 'react-router-dom'
-import JobseekerPage from './Pages/JobSeeker Page/JobseekerPage';
-import "./Pages/JobSeeker Page/JobSeeker_Page_index.css"
-import  ApplicationStatusComponent from  './Pages/JobSeeker Page/components/ApplicationStatusComponent'
-import UserSchduleComponent from "./Pages/JobSeeker Page/components/SideNavComponents/Schdeule/main"
-//////////////////////////////
+import {  Route, Routes } from "react-router-dom";
+import JobseekerPage from "./Pages/JobSeeker Page/JobseekerPage";
+import "./Pages/JobSeeker Page/JobSeeker_Page_index.css";
+import ApplicationStatusComponent from "./Pages/JobSeeker Page/components/ApplicationStatusComponent";
+import UserSchduleComponent from "./Pages/JobSeeker Page/components/SideNavComponents/Schdeule/main";
 
 import "./App.css";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import Landingpage from "./pages/HomePage/Landingpage";
-import CollegeAdmin from "./components/CollegeAdmin/CollegeAdmin";
+import LandingHome from "../src/components/Landingpage/LandingHome";
+import CollegeAdmin from "../src/components/CollegeAdmin/CollegeAdmin";
 
-const router = createBrowserRouter([
-  { path: "/", element: <Landingpage /> },
-  { path: "/collegeadmin", element: <CollegeAdmin /> },
-]);
 
-function App() {
-  return <RouterProvider router={router} />;
-}
-
-export default App;
-//////////////////////////////////////
 
 
 function App() {
-
-
-
-
   return (
     <div className="App">
       <Routes>
-        <Route
-          path="/"
-          element={
-            <div>
-              <h1>Hello</h1> <Link to="/user">USER </Link>
-            </div>
-          }
-        />
+        <Route path="/" element={<LandingHome />} />
+        <Route path="/collegeadmin" element={<CollegeAdmin />} />
         <Route path="/user" element={<JobseekerPage />}>
           <Route index element={<ApplicationStatusComponent />} />
           <Route path="schedule" element={<UserSchduleComponent />} />
