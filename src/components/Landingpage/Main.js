483c90fb30f0b0835784f792e4253eba64bf2195
Aswin-web <ashvinachu097@gmail.com>
2023-03-16 17:24:15 +0530

    update

--- src/components/Landingpage/Main.js
+++ src/components/Landingpage/Main.js
@@ -1,10 +1,18 @@
 import React from "react";
 import classes from "./Main.module.css";
 import { Button } from "@mui/material";
+// import { useCookies } from "react-cookie";
 const Main = () => {
+   
+   
   return (
     <div id="main" className={classes.main}>
+      
+     
+
       <main className={classes.container}>
+
+        
         <div className={classes.quote}>
           <h1>Track Your Application </h1>
           <p>
@@ -19,6 +27,7 @@ const Main = () => {
             Login{" "}
           </Button>
         </div>
+        
         <div className={classes.image}>
           <img
             src="https://images.ctfassets.net/pdf29us7flmy/4WkJUOsDpXs8FuziSMnP6T/82cb8b0cdf309bf3851621768011c1c8/Career_Guide_Photos-2160x1215-GettyImages-1166177942.jpg?w=720&q=100&fm=jpg"
